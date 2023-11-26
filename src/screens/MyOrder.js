f6910e63604ad326a5556f909e14711960b43269
Amit Saini <65627409+amitsaini144@users.noreply.github.com>
2023-11-25 18:45:11 -0800

    update: file

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react'
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
